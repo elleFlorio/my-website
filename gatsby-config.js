a2229f7d0965917591d27849e34f889849e20af1
Luca Florio <luca@pleo.io>
2020-12-06 16:48:39 +0100

    Update tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-PCGP4X5GE6",
+        trackingId: "UA-183802220-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
